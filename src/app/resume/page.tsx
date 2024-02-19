2294efa77a152334cb65b6e076fa38e13e95d2a3
Badar Usman <baderusman23@gmail.com>
2024-02-19 20:25:20 +0400

    chore(removed-next): deleted next related code

--- src/app/resume/page.tsx
+++
@@ -1,8 +0,0 @@
-import DataContainer from './DataContainer'
-export default function Home() {
-  return (
-    <main style={{width: '100vw', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-        <DataContainer />
-    </main>
-  );
-}
