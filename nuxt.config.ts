9642d9eb5944c9e36157c1e837cbdccc33e521e1
Badar Usman <baderusman23@gmail.com>
2024-02-22 19:25:55 +0400

    revert(scss-variables): removed custom scss variables config from vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,54 +1,50 @@
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import vuetifySass from '@paro-paro/vite-plugin-vuetify-sass'
+import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify'
 
-import { RuntimeModes } from "./types/enums";
+import {RuntimeModes} from "./types/enums";
 import i18nConfig from "./config/i18n";
 
 export default defineNuxtConfig({
-  devtools: { enabled: process.env.NODE_ENVIRONMENT === RuntimeModes.DEVELOPMENT },
-  build: {
-    transpile: ['vuetify'],
-  },
-  modules: [
-    '@nuxt/image',
-    [
-      '@nuxtjs/sitemap',
-      {
-        url: process.env.SITE_URL,
-        trailingSlash: true
-      }
-    ],
-    [
-      '@nuxtjs/i18n',
-      {
-        ...i18nConfig,
-        vueI18n: './i18n.config.ts',
-        baseUrl: process.env.SITE_URL,
-      }
-    ],
-    (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }))
-        config.plugins?.push(vuetifySass({
-          configFile: 'assets/styles/variables.scss',
-        }),)
-      })
+    devtools: {enabled: process.env.NODE_ENVIRONMENT === RuntimeModes.DEVELOPMENT},
+    build: {
+        transpile: ['vuetify'],
     },
-  ],
-  routeRules: {
-    // #TODO: add proper type here
-    ...(['/', '/builder'].map((routeItem) => ({[routeItem]: {prerender: true}})) as any)
-  },
-  typescript: {
-    typeCheck: true,
-    strict: true
-  },
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls,
-      },
+    modules: [
+        '@nuxt/image',
+        [
+            '@nuxtjs/sitemap',
+            {
+                url: process.env.SITE_URL,
+                trailingSlash: true
+            }
+        ],
+        [
+            '@nuxtjs/i18n',
+            {
+                ...i18nConfig,
+                vueI18n: './i18n.config.ts',
+                baseUrl: process.env.SITE_URL,
+            }
+        ],
+        (_options, nuxt) => {
+            nuxt.hooks.hook('vite:extendConfig', (config) => {
+                // @ts-expect-error
+                config.plugins.push(vuetify({autoImport: true}))
+            })
+        },
+    ],
+    routeRules: {
+        // #TODO: add proper type here
+        ...(['/', '/builder'].map((routeItem) => ({[routeItem]: {prerender: true}})) as any)
     },
-  },
+    typescript: {
+        typeCheck: true,
+        strict: true
+    },
+    vite: {
+        vue: {
+            template: {
+                transformAssetUrls,
+            },
+        },
+    },
 })
