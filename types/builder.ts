2e2a0766845953975611138b09cf978ddcbfb1f5
Badar Usman <baderusman23@gmail.com>
2024-03-02 16:24:59 +0400

    refactor(junk): removed some junk

--- types/builder.ts
+++ types/builder.ts
@@ -1,26 +1,25 @@
 export enum EDITOR_FIELDS {
-  TEXT = 'text',
-  START_AND_END_DATE = 'startAndEndDate',
-  EDITOR = 'editor'
+  TEXT = "text",
+  START_AND_END_DATE = "startAndEndDate",
+  EDITOR = "editor",
 }
 
 export type EditorFieldSchema = {
-  title: string,
-  type: EDITOR_FIELDS,
-  value: string,
-  fieldName: string
-  cols: number,
-}
+  title: string;
+  type: EDITOR_FIELDS;
+  value: string;
+  fieldName: string;
+  cols: number;
+};
 
 export type SectionItemTitleTemplate = { fieldName: string } | { text: string };
 
-
 export type Section = {
-  title: string,
-  subtitle: string,
-  formTemplate: EditorFieldSchema[],
-  type: string,
-  addActionText: string,
-  dataKey: string,
-  itemTitleTemplate: SectionItemTitleTemplate[]
-}
+  title: string;
+  subtitle: string;
+  formTemplate: EditorFieldSchema[];
+  type: string;
+  addActionText: string;
+  dataKey: string;
+  itemTitleTemplate: SectionItemTitleTemplate[];
+};
--- types/resumeData.ts
+++ types/resumeData.ts
@@ -1,56 +1,82 @@
 export type EmploymentHistoryItem = {
-    jobTitle: string;
-    employer: string;
-    startAndEndDate: string; // Consider Date objects for easier handling
-    city: string;
-    description: string; // Rich text placeholder
+  jobTitle: string;
+  employer: string;
+  startAndEndDate: string; // Consider Date objects for easier handling
+  city: string;
+  description: string; // Rich text placeholder
 };
 
 export type ProjectItem = {
-    projectName: string;
-    url: string;
-    startAndEndDate: string; // Consider Date objects
-    description: string; // Rich text placeholder
+  projectName: string;
+  url: string;
+  startAndEndDate: string; // Consider Date objects
+  description: string; // Rich text placeholder
 };
 
 export type EducationItem = {
-    school: string;
-    degree: string;
-    startAndEndDate: string; // Consider Date objects
-    city: string;
-    description: string; // Rich text placeholder
+  school: string;
+  degree: string;
+  startAndEndDate: string; // Consider Date objects
+  city: string;
+  description: string; // Rich text placeholder
 };
 
 export type SocialLinkItem = {
-    label: string;
-    url: string;
+  label: string;
+  url: string;
 };
 
 export type SkillItem = {
-    skillName: string;
-    expertiseLevel: number; // Numeric type likely better
+  skillName: string;
+  expertiseLevel: number; // Numeric type likely better
 };
 
 export type LanguageItem = {
-    language: string;
-    fluency: string; // Or potentially an enum of fluency levels
+  language: string;
+  fluency: string; // Or potentially an enum of fluency levels
 };
 
 export type CourseItem = {
-    courseName: string;
-    institution: string;
-    startAndEndDate: string; // Consider Date objects
+  courseName: string;
+  institution: string;
+  startAndEndDate: string; // Consider Date objects
 };
 
 export type ExtraCurricularActivityItem = {
-    activityTitle: string;
-    startAndEndDate: string; // Consider Date objects
-    description: string;  // Rich text placeholder
+  activityTitle: string;
+  startAndEndDate: string; // Consider Date objects
+  description: string; // Rich text placeholder
 };
 
 export type ReferenceItem = {
-    referenceFullName: string;
-    company: string;
-    phone: string;
-    email: string;
+  referenceFullName: string;
+  company: string;
+  phone: string;
+  email: string;
+};
+
+type ResumeInformation = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  country: string;
+  city: string;
+  address: string;
+  postalCode: string;
+  drivingLicense: string;
+  nationality: string;
+  placeOfBirth: string;
+  dateOfBirth: string;
+  professionalSummary: string; // You may need to use a specific rich text type here
+  employmentHistory: EmploymentHistoryItem[];
+  projects: ProjectItem[];
+  education: EducationItem[];
+  socialLinks: SocialLinkItem[];
+  skills: SkillItem[];
+  languages: LanguageItem[];
+  courses: CourseItem[];
+  hobbies: string;
+  extraCurricularActivities: ExtraCurricularActivityItem[];
+  references: ReferenceItem[];
 };
