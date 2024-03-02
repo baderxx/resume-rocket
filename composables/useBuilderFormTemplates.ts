05dae949d25c595f0bd9d7247c65dff5e971f9f9
Badar Usman <baderusman23@gmail.com>
2024-03-02 18:43:30 +0400

    feat(sections): added more sections to the builder

--- composables/useBuilderFormTemplates.ts
+++ composables/useBuilderFormTemplates.ts
@@ -175,6 +175,112 @@ export const useUseBuilderFormTemplates = () => {
     ];
   });
 
+  const CourcesFormTemplate = computed<EditorFieldSchema[]>(() => {
+    return [
+      {
+        title: "Cource",
+        type: EDITOR_FIELDS.TEXT,
+        value: "",
+        fieldName: "courseName",
+        cols: 6,
+      },
+      {
+        title: "Institution",
+        type: EDITOR_FIELDS.START_AND_END_DATE,
+        value: "",
+        fieldName: "institution",
+        cols: 6,
+      },
+      {
+        title: "Start & End Date",
+        type: EDITOR_FIELDS.START_AND_END_DATE,
+        value: "",
+        fieldName: "startAndEndDate",
+        cols: 6,
+      },
+    ];
+  });
+
+  const hobbiesFormTemplate = computed<EditorFieldSchema[]>(() => {
+    return [
+      {
+        title: "Hobbies",
+        type: EDITOR_FIELDS.TEXT,
+        value: "",
+        fieldName: "hobbies",
+        cols: 12,
+      },
+    ];
+  });
+
+  const extraCurricularActivitiesFormTemplate = computed<EditorFieldSchema[]>(
+    () => {
+      return [
+        {
+          title: "Activity title",
+          type: EDITOR_FIELDS.TEXT,
+          value: "",
+          fieldName: "activityTitle",
+          cols: 6,
+        },
+        {
+          title: "Start & End Date",
+          type: EDITOR_FIELDS.START_AND_END_DATE,
+          value: "",
+          fieldName: "startAndEndDate",
+          cols: 6,
+        },
+        {
+          title: "City",
+          type: EDITOR_FIELDS.TEXT,
+          value: "",
+          fieldName: "city",
+          cols: 6,
+        },
+        {
+          title: "Description",
+          type: EDITOR_FIELDS.EDITOR,
+          value: "",
+          fieldName: "description",
+          cols: 12,
+        },
+      ];
+    },
+  );
+
+  const referenceFormTemplate = computed<EditorFieldSchema[]>(() => {
+    return [
+      {
+        title: "Referent's Full Name",
+        type: EDITOR_FIELDS.TEXT,
+        value: "",
+        fieldName: "referenceFullName",
+        cols: 6,
+      },
+      {
+        title: "Company",
+        type: EDITOR_FIELDS.TEXT,
+        value: "",
+        fieldName: "company",
+        cols: 6,
+      },
+      {
+        title: "Phone",
+        type: EDITOR_FIELDS.TEXT,
+        value: "",
+        fieldName: "phone",
+        cols: 6,
+      },
+      {
+        title: "Email",
+        type: EDITOR_FIELDS.TEXT,
+        value: "",
+        fieldName: "email",
+        cols: 6,
+      },
+    ];
+  });
+
   const experienceSection = computed<Section>(() => ({
     title: sectionTitle.value,
     subtitle: sectionSubtitle.value,
@@ -243,6 +349,60 @@ export const useUseBuilderFormTemplates = () => {
     itemTitleTemplate: [{ fieldName: "language" }],
   }));
 
+  const internshipSection = computed<Section>(() => ({
+    title: "Internships",
+    subtitle: sectionSubtitle.value,
+    formTemplate: experienceFormTemplate.value, // #TODO: Add inline schema for section
+    type: "some type", // #TODO: figure this out
+    addActionText: "Add one more internship",
+    dataKey: "internships",
+    itemTitleTemplate: [
+      { fieldName: "jobTitle" },
+      { text: "at" },
+      { fieldName: "employer" },
+    ],
+  }));
+
+  const courcesSection = computed<Section>(() => ({
+    title: "Cources",
+    subtitle: sectionSubtitle.value,
+    formTemplate: CourcesFormTemplate.value, // #TODO: Add inline schema for section
+    type: "some type", // #TODO: figure this out
+    addActionText: "Add one more cource",
+    dataKey: "courses",
+    itemTitleTemplate: [{ fieldName: "courseName" }],
+  }));
+
+  const hobbiesSection = computed<Section>(() => ({
+    title: "Hobbies",
+    subtitle: sectionSubtitle.value,
+    formTemplate: hobbiesFormTemplate.value, // #TODO: Add inline schema for section
+    type: "some type", // #TODO: figure this out
+    addActionText: "Add one more hobby",
+    dataKey: "hobbies",
+    itemTitleTemplate: [{ fieldName: "courseName" }],
+  }));
+
+  const extraCurricularActivitiesSection = computed<Section>(() => ({
+    title: "Extra-curricular Activities",
+    subtitle: sectionSubtitle.value,
+    formTemplate: extraCurricularActivitiesFormTemplate.value, // #TODO: Add inline schema for section
+    type: "some type", // #TODO: figure this out
+    addActionText: "Add one more activity",
+    dataKey: "extraCurricularActivities",
+    itemTitleTemplate: [{ fieldName: "activityTitle" }],
+  }));
+
+  const referencesSection = computed<Section>(() => ({
+    title: "References",
+    subtitle: sectionSubtitle.value,
+    formTemplate: referenceFormTemplate.value, // #TODO: Add inline schema for section
+    type: "some type", // #TODO: figure this out
+    addActionText: "Add one more reference",
+    dataKey: "references",
+    itemTitleTemplate: [{ fieldName: "referenceFullName" }],
+  }));
+
   const builderSections = computed<Section[]>(() => {
     return [
       experienceSection.value,
@@ -251,6 +411,11 @@ export const useUseBuilderFormTemplates = () => {
       websiteAndSocialLinksSection.value,
       skillsSection.value,
       languaeSection.value,
+      internshipSection.value,
+      courcesSection.value,
+      hobbiesSection.value,
+      extraCurricularActivitiesSection.value,
+      referencesSection.value,
     ];
   });
 
